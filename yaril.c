ac465dbc88c7b84c8286d3a70ff2b9f9beecfaba
yarilsalinas <yaril.salinas.r@mail.pucv.cl>
2026-05-08 23:10:29 +0000

    Agregué función insertar

---
+++ yaril.c
@@ -0,0 +1,13 @@
+#include "tdas/extra.h"
+#include "tdas/list.h"
+#include "tdas/map.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char id[100];
+  char title[100];
+  List *genres;
+  int year;
+} Film;
