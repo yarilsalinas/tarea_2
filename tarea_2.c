e532800e9dca010cb415a7dd7b77cbb022e267b1
yarilsalinas <yaril.salinas.r@mail.pucv.cl>
2026-05-10 01:07:14 +0000

    buscar por decada listo

--- tarea_2.c
+++ tarea_2.c
@@ -22,7 +22,7 @@ void mostrarMenuPrincipal() {
   puts("1) Cargar Películas");
   puts("2) Buscar por género");
   puts("3) Buscar por autor");
-  puts("4) ...");
+  puts("4) Buscar por decada");
   puts("5) ...");
   puts("6) ...");
   puts("7) ...");
@@ -42,74 +42,75 @@ void imprimir_lista_strings(List *lista) {
     while (item != NULL) {
         printf("%s", item);
         item = list_next(lista);
-        if (item != NULL) printf(", "); // Separa con coma si hay más
+        if (item != NULL) printf(", ");
     }
     printf("\n");
 }
 
-void cargar_peliculas(Map *pelis_byid, Map *pelis_bygenres, Map *pelis_bydirectors) { 
-    FILE *archivo = fopen("Top1500.csv", "r");
-    if (archivo == NULL) {
-      perror(
-          "Error al abrir el archivo"); 
-      return;
-    }
-    char **campos;
+void cargar_peliculas(Map *pelis_byid, Map *pelis_bygenres, Map *pelis_bydirectors, Map *pelis_bydecada) { 
+  FILE *archivo = fopen("Top1500.csv", "r");
+  if (archivo == NULL) {
+    perror(
+        "Error al abrir el archivo"); 
+    return;
+  }
+  char **campos;
     // Leer y parsear una línea del archivo CSV. La función devuelve un array de
     // strings, donde cada elemento representa un campo de la línea CSV procesada.
-    campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
+  campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
 
     // Lee cada línea del archivo CSV hasta el final
-    while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
-      // Crea una nueva estructura Film y almacena los datos de cada película
-      Film *peli = (Film *)malloc(sizeof(Film));
-      strcpy(peli->id, campos[1]);        // Asigna ID
-      strcpy(peli->title, campos[5]);     // Asigna título
-      peli->directors = split_string(campos[14], ",");// Asigna director
-      peli->genres = split_string(campos[11], ",");       // Inicializa la lista de géneros
-      peli->year = atoi(campos[10]); // Asigna año, convirtiendo de cadena a entero
+  while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
+    Film *peli = (Film *)malloc(sizeof(Film));// Crea una nueva estructura Film y almacena los datos de cada película
+    strcpy(peli->id, campos[1]);        // Asigna ID
+    strcpy(peli->title, campos[5]);     // Asigna título
+    peli->directors = split_string(campos[14], ",");// Asigna director
+    peli->genres = split_string(campos[11], ",");       // Inicializa la lista de géneros
+    peli->year = atoi(campos[10]); // Asigna año, convirtiendo de cadena a entero
+    
+    map_insert(pelis_byid, peli->id, peli);// Inserta la película en el mapa usando el ID como clave
+    char *genre = list_first(peli->genres);// Obtiene el primer género de la lista de géneros de la película  
+    while (genre != NULL) {// Itera sobre cada género de la película
+      MapPair *genre_pair = map_search(pelis_bygenres, genre);// Busca el género en el mapa pelis_bygenres
+      if (genre_pair == NULL) {// Si el género no existe en el mapa, crea una nueva lista y agrégala al mapa
+        List *new_list = list_create();
+        list_pushBack(new_list, peli);
+        map_insert(pelis_bygenres, genre, new_list);
+      } else {// Si el género ya existe en el mapa, obtén la lista y agrega la película
+          List *genre_list = (List *)genre_pair->value;
+          list_pushBack(genre_list, peli);
+        }
+      genre = list_next(peli->genres);// Avanza al siguiente género en la lista
+    }
 
-      // Inserta la película en el mapa usando el ID como clave
-      map_insert(pelis_byid, peli->id, peli);
-
-      // Obtiene el primer género de la lista de géneros de la película
-      char *genre = list_first(peli->genres);
-      // Itera sobre cada género de la película
-      while (genre != NULL) {
-          // Busca el género en el mapa pelis_bygenres
-          MapPair *genre_pair = map_search(pelis_bygenres, genre);
-
-          // Si el género no existe en el mapa, crea una nueva lista y agrégala al mapa
-          if (genre_pair == NULL) {
-              List *new_list = list_create();
-              list_pushBack(new_list, peli);
-              map_insert(pelis_bygenres, genre, new_list);
-          } else {
-              // Si el género ya existe en el mapa, obtén la lista y agrega la película
-              List *genre_list = (List *)genre_pair->value;
-              list_pushBack(genre_list, peli);
-          }
-
-          // Avanza al siguiente género en la lista
-          genre = list_next(peli->genres);
+    char *director = list_first(peli->directors);
+    while (director != NULL) {
+      MapPair *director_pair = map_search(pelis_bydirectors, director);
+      if (director_pair == NULL) {
+        List *new_list = list_create();
+        list_pushBack(new_list, peli);
+        map_insert(pelis_bydirectors, director, new_list);
+        } else {
+            List *director_list = (List *)director_pair->value;
+            list_pushBack(director_list, peli);
+        }
+        director = list_next(peli->directors);
       }
 
-      char *director = list_first(peli->directors);
-      while (director != NULL) {
-          MapPair *director_pair = map_search(pelis_bydirectors, director);
+    int decada_val = (peli->year / 10) * 10;  
+    char *decada_key = malloc(5 * sizeof(char));
+    sprintf(decada_key, "%d", decada_val); 
+    MapPair *decada_pair = map_search(pelis_bydecada, decada_key);
 
-          if (director_pair == NULL) {
-              List *new_list = list_create();
-              list_pushBack(new_list, peli);
-              map_insert(pelis_bydirectors, director, new_list);
-          } else {
-              List *director_list = (List *)director_pair->value;
-              list_pushBack(director_list, peli);
-          }
-
-          director = list_next(peli->directors);
-      }
-
+    if (decada_pair == NULL) { 
+      List *new_list = list_create();
+      list_pushBack(new_list, peli);
+      map_insert(pelis_bydecada, decada_key, new_list);
+    } else { 
+        List *decada_list = (List *)decada_pair->value;
+        list_pushBack(decada_list, peli); 
+        free(decada_key); 
+    }
     }
     printf("archivo cargado correctamente\n");
     fclose(archivo); // Cierra el archivo después de leer todas las líneas
@@ -167,12 +168,38 @@ void buscar_por_director(Map *pelis_bydirectors) {
   printf("---------------------------\n");
 }
 
+void buscar_por_decada(Map *pelis_bydecada){
+  char decada[10];
+  printf("Ingrese la decada a buscar: ");
+  scanf("%s", decada);
+
+  MapPair *pair = map_search(pelis_bydecada, decada);
+  if (pair == NULL) {
+      printf("\nNo se encontraron peliculas para la decada: %s\n", decada);
+      return;
+  }
+
+  List *lista = (List *)pair->value;
+  Film *peli = list_first(lista);
+
+  printf("\nResultados para la decada '%s':\n", decada);
+  while (peli != NULL) {
+    printf("---------------------------\n");
+    printf("ID     : %s\n", peli->id);
+    printf("Titulo : %s\n", peli->title);
+    printf("Fecha  : %d\n", peli->year);
+    peli = list_next(lista);
+  }
+  printf("---------------------------\n");
+}
+
 int main() {
   char opcion;
   Map *pelis_byid = map_create(is_equal_str);
   Map *pelis_bygenres = map_create(is_equal_str);
   Map *pelis_bydirectors = map_create(is_equal_str);
-
+  Map *pelis_bydecada = map_create(is_equal_str);
+  
   do {
     mostrarMenuPrincipal();
     printf("Ingrese su opción: ");
@@ -180,7 +207,7 @@ int main() {
 
     switch (opcion) {
     case '1':
-      cargar_peliculas(pelis_byid, pelis_bygenres, pelis_bydirectors);
+      cargar_peliculas(pelis_byid, pelis_bygenres, pelis_bydirectors, pelis_bydecada);
       break;
     case '2':
       buscar_por_genero(pelis_bygenres);
@@ -189,8 +216,7 @@ int main() {
       buscar_por_director(pelis_bydirectors);
       break;
     case '4':
-      //Buscar por Década: La usuaria ingresa un año de referencia (ej. 1990) y la aplicación debe mostrar todas las películas lanzadas en esa década (en este caso, entre 1990 y 1999 inclusive).
-      
+      buscar_por_decada(pelis_bydecada);
       break;
     case '5':
       //Búsqueda Avanzada (Doble Criterio): La aplicación solicita a la usuaria un género y una década simultáneamente. El sistema debe mostrar solo los títulos que cumplan con ambos requisitos (ej. "Terror" de los "1990").
